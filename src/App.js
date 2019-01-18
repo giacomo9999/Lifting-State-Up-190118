fe4a4c15925a0356d9a7abe9bdee0adeacc60113
James Gary <gmarchesi888@gmail.com>
2019-01-18 09:49:34 -0500

    Base case with one input working.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
-import { Container, Card } from "semantic-ui-react";
+import { Container, Card, Form, Button, Input } from "semantic-ui-react";
 import BoilingVerdict from "./components/boilingVerdict.component";
 
 class App extends Component {
+  state = { temperature: "" };
+
+  handleChange = e => {
+    this.setState({ temperature: e.target.value });
+  };
+
   render() {
+    const temperature = this.state.temperature;
     return (
       <Container>
-        <style>{`html, body {background-color: #2E4053 !important;}}`}</style>
+        <style>{`html, body {background-color: #2E4053 !important;} }`}</style>
 
         <br />
         <Card centered>
           <Card.Content>
             <Card.Header textAlign="center">Temperature Calculator</Card.Header>
+            <br />
+            <Form.Field>
+              <Input
+                fluid
+                placeholder="Enter temp in Celsius:"
+                value={temperature}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <br />
 
-            <BoilingVerdict celsius="125"/>
+            <BoilingVerdict celsius={Number(temperature)} />
           </Card.Content>
         </Card>
       </Container>
